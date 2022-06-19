a1526cb96016d4db3309650fe9fc3043ba7bb4ba
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-06-19 18:26:35 -0400

    Accept flags, remove comments

--- cmd/example-ping/main.go
+++ cmd/example-ping/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
+	"flag"
 	"fmt"
 	"io"
 	"log"
-	"net"
-	"net/http"
-	"net/url"
 	"sync"
 	"time"
 
@@ -18,36 +16,23 @@ import (
 	"golang.org/x/net/http2/hpack"
 )
 
-/*
-
-err := http2.ConfigureTransport(tr)
-
-client := http.Client{Transport: tr}
-r, err := client.Get(fmt.Sprintf("https://localhost:%d", port))
-
-c <- os.Interrupt
-
-if err != nil {
-	t.Fatalf("Error encountered while connecting to test server: %s", err)
-}
-
-if r.Proto != "HTTP/2.0" {
-	t.Fatalf("Expected HTTP/2 connection to server, but connection was using %s", r.Proto)
-}
-*/
+var (
+	host = flag.String("host", "", "hostname of target http2 server")
+	addr = flag.String("addr", "443", "port number to connect to")
+)
 
 var (
 	ready = make(chan bool)
 )
 
-func connect(host, addr string) (*http2.Framer, error) {
+func connect(host, port string) (*http2.Framer, error) {
 	cfg := &tls.Config{
 		ServerName:         host,
 		NextProtos:         []string{"h2", "h2-14"},
-		InsecureSkipVerify: false,
+		InsecureSkipVerify: true,
 	}
 
-	hostAndPort := addr
+	hostAndPort := host + ":" + port
 	log.Printf("Connecting to %s ...", hostAndPort)
 	tc, err := tls.Dial("tcp", hostAndPort, cfg)
 	if err != nil {
@@ -71,7 +56,7 @@ func connect(host, addr string) (*http2.Framer, error) {
 	}
 
 	// Start HTTP/2 connection.
-	log.Printf("Sending client preface: %q", http2.ClientPreface)
+	log.Printf("SEND preface: %q", http2.ClientPreface)
 	if _, err := io.WriteString(tc, http2.ClientPreface); err != nil {
 		return nil, err
 	}
@@ -79,10 +64,10 @@ func connect(host, addr string) (*http2.Framer, error) {
 	framer := http2.NewFramer(tc, tc)
 	framer.SetMaxReadFrameSize((1 << 24) - 1)
 	s := []http2.Setting{
-		http2.Setting{ID: http2.SettingMaxFrameSize, Val: (1 << 17)},
+		http2.Setting{ID: http2.SettingMaxFrameSize, Val: (1 << 24) - 1},
 		http2.Setting{ID: http2.SettingInitialWindowSize, Val: (1 << 30) - 1},
 	}
-	log.Println("Sending settings: ", s)
+	log.Println("SEND settings: ", s)
 	err = framer.WriteSettings(s...)
 	rtx.Must(err, "failed to write settings")
 	//log.Println("Sending settings ack (presumptively)")
@@ -92,7 +77,7 @@ func connect(host, addr string) (*http2.Framer, error) {
 }
 
 func request(framer *http2.Framer) {
-	log.Println("Waiting for SETTINGS(ack)")
+	log.Println("WAIT - SETTINGS(ack)")
 	<-ready // block until pinger receives frame.
 
 	buf := bytes.Buffer{}
@@ -108,30 +93,18 @@ func request(framer *http2.Framer) {
 		EndStream:     false,
 		EndHeaders:    true,
 	}
-	log.Println("Sending headers request: ", p, string(p.BlockFragment))
+	log.Println("SEND - request header: ", p)
 	err := framer.WriteHeaders(p)
 	rtx.Must(err, "failed to write headers for stream 1")
 
-	log.Println("WriteWindowUpdate")
-	err = framer.WriteWindowUpdate(0, 1<<30)
+	log.Println("SEND - WriteWindowUpdate(0)")
+	err = framer.WriteWindowUpdate(0, (1<<30)-1)
 	rtx.Must(err, "failed to update window 0")
-	err = framer.WriteWindowUpdate(1, 1<<30)
+	err = framer.WriteWindowUpdate(1, (1<<30)-1)
 	rtx.Must(err, "failed to update window 1")
-
-	//return framer, nil
 }
 
 func ping(framer *http2.Framer) {
-	// err := framer.WriteSettingsAck()
-	// rtx.Must(err, "failed to write settings ack")
-	/*
-		s := []http2.Setting{
-			http2.Setting{ID: http2.SettingInitialWindowSize, Val: (2 << 21)},
-			http2.Setting{ID: http2.SettingMaxFrameSize, Val: (2 << 22)},
-		}
-		err = framer.WriteSettings(s...)
-		rtx.Must(err, "failed to write settings")
-	*/
 	total := 0
 	t1 := time.Now()
 	start := time.Now()
@@ -142,28 +115,32 @@ func ping(framer *http2.Framer) {
 			rtx.Must(err, "failed to read frame")
 			switch f := f.(type) {
 			case *http2.PingFrame:
-				log.Printf("  PING = %q %s %f Mbps %f MB %f sec\n",
+				log.Printf("  RECV PING = %q %s %f Mbps %f MB %f sec\n",
 					f.Data, time.Since(t1),
-					float64(total)/time.Since(start).Seconds()/1000/1000,
+					8*float64(total)/time.Since(start).Seconds()/1000/1000,
 					float64(total)/1000/1000,
 					time.Since(start).Seconds())
 			case *http2.DataFrame:
-				fmt.Println("  DATA = ", time.Since(t1), f.Length, f.Type, f.StreamID, f.StreamEnded(), f.FrameHeader.String())
-				// fmt.Println(string(f.Data()))
 				total += int(f.Length)
-				err = framer.WriteWindowUpdate(1, f.Length)
-				if err != nil {
-					log.Println("  - window update:", err)
+				// fmt.Println("  DATA = ", time.Since(t1), f.Length, f.Type, "stream:", f.StreamID, f.StreamEnded(), f.FrameHeader.String(), total)
+				if f.StreamEnded() {
+					break
 				}
+				// fmt.Println(string(f.Data()))
+				// TODO: why are both necessary?
 				err = framer.WriteWindowUpdate(0, f.Length)
 				if err != nil {
 					log.Println("  - window update:", err)
 				}
+				err = framer.WriteWindowUpdate(f.StreamID, f.Length)
+				if err != nil {
+					log.Println("  - window update:", err)
+				}
 			case *http2.SettingsFrame:
-				log.Println("  SETTINGS = ", time.Since(t1), f.String())
+				log.Println("  RECV SETTINGS = ", time.Since(t1), f.String())
 				f.ForeachSetting(func(s http2.Setting) error { log.Println("  - ", s); return nil })
 				if !f.IsAck() {
-					log.Println("Sending SETTINGS(ack) ...")
+					log.Println("SEND - SETTINGS(ack) ...")
 					err = framer.WriteSettingsAck()
 					rtx.Must(err, "failed to write settings ack")
 				}
@@ -172,9 +149,9 @@ func ping(framer *http2.Framer) {
 					//log.Println("ready")
 				})
 			case *http2.WindowUpdateFrame:
-				log.Println("  WINDOW_UPDATE = ", f.String(), f.Increment)
+				log.Println("  RECV - WINDOW_UPDATE = ", f.String(), f.Increment)
 			case *http2.HeadersFrame:
-				log.Println("  HEADERS = ", f.String())
+				log.Println("  RECV - HEADERS = ", f.String())
 				b := f.HeaderBlockFragment()
 				dec := hpack.NewDecoder(4096, nil)
 				dec.SetEmitFunc(func(hf hpack.HeaderField) {
@@ -183,8 +160,7 @@ func ping(framer *http2.Framer) {
 				dec.Write(b)
 				rtx.Must(dec.Close(), "closed decoder")
 			default:
-				log.Println("frame:", f)
-				// pretty.Print(f)
+				log.Printf("RECV - %T %s", f, f)
 				log.Println()
 			}
 		}
@@ -193,92 +169,62 @@ func ping(framer *http2.Framer) {
 	var msg [8]byte
 	copy(msg[:], "h2i_ping")
 
-	/*	f, err := framer.ReadFrame()
-		fmt.Println(f)
-		rtx.Must(err, "failed to read 1 frame ")
-
-		f, err = framer.ReadFrame()
-		fmt.Println(f)
-		rtx.Must(err, "failed to read 2 frames")
-
-		err = framer.WritePing(false, msg)
-		rtx.Must(err, "failed to write ping")
-	*/
-
-	//f, err = framer.ReadFrame()
-	//rtx.Must(err, "failed to read response")
-	//fmt.Println(f)
 	var err error
 	for {
-		//err = framer.WriteWindowUpdate(1, 2^20)
-		//fmt.Println("window update:", err)
-
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		t1 = time.Now()
 		err = framer.WritePing(false, msg)
 		rtx.Must(err, "failed to write ping")
-		//fmt.Println("  loop = ", time.Since(t1))
 	}
 }
 
-/*
-	f(":authority", host)
-	m := req.Method
-	if m == "" {
-		m = MethodGet
-	}
-	f(":method", m)
-	if req.Method != "CONNECT" {
-		f(":path", path)
-		f(":scheme", req.URL.Scheme)
-	}
-*/
-
 func main() {
-
+	flag.Parse()
 	fmt.Println("ok")
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	err := http2.ConfigureTransport(tr)
-	rtx.Must(err, "failed to configure http transport")
 
-	req := &http.Request{
-		Method: "CONNECT",
-		URL: &url.URL{
-			Scheme: "https",
-			Host:   "mlab1-lga0t.mlab-sandbox.measurement-lab.org:443",
-		},
-	}
-
-	f, err := connect("mlab1-lga0t.mlab-sandbox.measurement-lab.org", "mlab1-lga0t.mlab-sandbox.measurement-lab.org:443")
+	// f, err := connect("mlab1-lga0t.mlab-sandbox.measurement-lab.org", "443")
+	f, err := connect(*host, *addr)
 	rtx.Must(err, "failed to connect")
-	// go read(f)
 	go request(f)
 	ping(f)
 
-	client := http.Client{Transport: tr}
-	// r, err := client.Get("https://ndt-mlab1-lga0t.mlab-sandbox.measurement-lab.org:443")
-	r, err := client.Do(req)
-	rtx.Must(err, "failed to get url")
-	_, ok := r.Body.(net.Conn)
-	if !ok {
-		panic("not a net.Conn")
-	}
-	//c, err := net.Dial("tcp", "ndt-mlab1-lga0t.mlab-sandbox.measurement-lab.org:443")
-	//c, err := net.Dial("tcp", "mlab-ns.appspot.com:80")
-	// rtx.Must(err, "failed to dail node.")
-	if r.Proto != "HTTP/2.0" {
-		fmt.Println("wrong proto: ", r.Proto)
-	}
-	fmt.Println("ok")
-	//cc, err := tr2.NewClientConn(c)
-	//rtx.Must(err, "failed to create tr2 client conn")
+	/*
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		err := http2.ConfigureTransport(tr)
+		rtx.Must(err, "failed to configure http transport")
+	*/
+	/*
+		req := &http.Request{
+			Method: "CONNECT",
+			URL: &url.URL{
+				Scheme: "https",
+				Host:   "mlab1-lga0t.mlab-sandbox.measurement-lab.org:443",
+			},
+		}
 
+		client := http.Client{Transport: tr}
+		// r, err := client.Get("https://ndt-mlab1-lga0t.mlab-sandbox.measurement-lab.org:443")
+		r, err := client.Do(req)
+		rtx.Must(err, "failed to get url")
+		_, ok := r.Body.(net.Conn)
+		if !ok {
+			panic("not a net.Conn")
+		}
+		//c, err := net.Dial("tcp", "ndt-mlab1-lga0t.mlab-sandbox.measurement-lab.org:443")
+		//c, err := net.Dial("tcp", "mlab-ns.appspot.com:80")
+		// rtx.Must(err, "failed to dail node.")
+		if r.Proto != "HTTP/2.0" {
+			fmt.Println("wrong proto: ", r.Proto)
+		}
+		fmt.Println("ok")
+		//cc, err := tr2.NewClientConn(c)
+		//rtx.Must(err, "failed to create tr2 client conn")
+	*/
 	/*
 		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 		defer cancel()
-
 		for err == nil {
 			t1 := time.Now()
 			err = cc.Ping(ctx)
